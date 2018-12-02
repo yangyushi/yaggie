277857efb224422a33b90d15325e45b697272240
Yushi Yang <yangyushi1992@icloud.com>
2018-12-02 15:23:45 +0000

    remove mini-trackpy linker, use only trackpy instead

--- yaggie/engine/track.py
+++ yaggie/engine/track.py
@@ -2,38 +2,6 @@ import numpy as np
 import trackpy as tp
 
 
-class MinimumTrackpyEngine():
-    def __init__(self, max_movement):
-        """
-        unit of the movement is pixel
-        """
-        self.max_movement = max_movement
-
-    def __get_trajectory(self, index, labels, maxima):
-        """
-        trajectory: [(x1, y1, z1) --> t1, (x2, y2, z2) --> t2, ... ]
-        """
-        trajectory = []
-        for i, time in enumerate(labels):
-            for j, label in enumerate(time):
-                if label == index:
-                    trajectory.append(np.array(maxima[i][j]))
-        return np.array(trajectory)
-
-    def __get_trajectories(self, labels, maxima):
-        result = []
-        for i in range(labels.flatten().max() + 1):
-            result.append(self.__get_trajectory(i, labels, maxima))
-        return np.array(result)
-
-    def run(self, maxima):
-        link_result = tp.link_iter(maxima, self.max_movement)
-        labels = np.array([np.array(r[1]) for r in link_result])
-        trajectories = self.__get_trajectories(labels, maxima)
-        self.trajectories = trajectories
-        return trajectories
-
-
 class TrackpyEngine():
     def __init__(self, max_movement, memory=0):
         """
